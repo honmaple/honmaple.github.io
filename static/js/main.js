4bd122761150f8d779816e1040a9805d2443f2b0
honmaple <xiyang0807@gmail.com>
2021-03-29 02:05:21 +0800

    update

--- static/js/main.js
+++ static/js/main.js
@@ -1,18 +1,3 @@
-function dispatch() {
-    var q = document.getElementById("search");
-    if (q.value !== "") {
-        var url = 'https://www.google.com/search?q=site:honmaple.me%20' + q.value;
-        if (navigator.userAgent.indexOf('iPad') > -1 || navigator.userAgent.indexOf('iPod') > -1 || navigator.userAgent.indexOf('iPhone') > -1) {
-            location.href = url;
-        } else {
-            window.open(url, "_blank");
-        }
-        return false;
-    } else {
-        return false;
-    }
-}
-
 function decrypt(_this) {
     var _next = _this.next();
     if (_this.prev().val() === "" ){
@@ -39,7 +24,7 @@ $(document).ready(function(){
     $(window).on("scroll", function() {
         var offsetHeight = document.body.offsetHeight;
         var scrollHeight = document.body.scrollHeight;
-        var navbarFixed = scrollHeight > offsetHeight && scrollHeight - offsetHeight > 243;
+        var navbarFixed = scrollHeight > offsetHeight && scrollHeight - offsetHeight > 245;
         e.toggleClass("back-to-top-on", window.pageYOffset > 120);
         if (navbarFixed) {
             $(".navbar").toggleClass("navbar-fixed", window.pageYOffset > 120);
@@ -74,9 +59,17 @@ $(document).ready(function(){
     });
 
     $(".entry-comment").click(function() {
-        var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
-        dsq.src = 'https://honmaple.disqus.com/embed.js';
-        (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+        var dsq = document.createElement('script');
+        var thr = document.getElementById('comment_thread')
+        dsq.type = 'text/javascript';
+        dsq.src = 'https://utteranc.es/client.js';
+        dsq.setAttribute('repo', 'honmaple/honmaple.github.io');
+        dsq.setAttribute('issue-term', thr.getAttribute('issue-term'));
+        dsq.setAttribute('label', 'comment');
+        dsq.setAttribute('theme', 'github-light');
+        dsq.setAttribute('crossorigin', 'anonymous');
+
+        thr.appendChild(dsq);
         $(".entry-comment").parent().fadeToggle(800);
     });
     $(".entry-theme").click(function() {
@@ -88,8 +81,7 @@ $(document).ready(function(){
     });
 
     $('#entry-decrypt-password').keyup(function(e){
-        if(e.keyCode == 13)
-        {
+        if(e.keyCode == 13) {
             decrypt($(this).next());
         }
     });
