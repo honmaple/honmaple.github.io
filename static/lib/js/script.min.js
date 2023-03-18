84a22d717200bd5d9ebcee686afef3368f557e38
honmaple <xiyang0807@gmail.com>
2023-03-18 22:41:12 +0800

    update

--- static/lib/js/script.min.js
+++ static/lib/js/script.min.js
@@ -1,18 +1,34 @@
-$(document).ready(function(){
-    $("pre").css("max-height", $(window).height()/1.5);
+$(document).ready(function () {
+    $("pre").css("max-height", $(window).height() / 1.5);
     var e = $(".back-to-top");
-    $(window).on("scroll", function() {
-        var offsetHeight = document.body.offsetHeight;
-        var scrollHeight = document.body.scrollHeight;
-        var navbarFixed = scrollHeight > offsetHeight && scrollHeight - offsetHeight > 243;
+    var titles = document.getElementsByClassName("entry-title");
+    $(window).on("scroll", function () {
+        var navbar = $(".navbar");
+        var height = $(".navbar").height();
+        var offset = Math.min(document.body.scrollHeight - document.body.offsetHeight, height);
+
         e.toggleClass("back-to-top-on", window.pageYOffset > 120);
-        if (navbarFixed) {
-            $(".navbar").toggleClass("navbar-fixed", window.pageYOffset > 120);
-            if (!$("#navbar-title").length && window.pageYOffset > 120 && window.innerWidth > 1280 && $(".entry-title").length == 1) {
-                $(".navbar-nav").prepend('<li id="navbar-title"><a>' + $(".entry-title").text() + '</a></li>');
-            }
-            if ($("#navbar-title").length && window.pageYOffset < 120) {
-                $("#navbar-title").remove();
+        navbar.toggleClass("navbar-fixed", window.pageYOffset > offset);
+
+        if (window.innerWidth > 1280) {
+            var scrollTop = window.pageYOffset;
+            var currentTitle = document.getElementById("navbar-title");
+            if (scrollTop > height) {
+                for (var i = titles.length - 1; i >= 0; i--) {
+                    if (scrollTop > titles[i].offsetTop - titles[i].offsetHeight) {
+                        if (!currentTitle) {
+                            currentTitle = document.createElement("li");
+                            currentTitle.id = "navbar-title"
+                            $(".navbar-nav").prepend(currentTitle);
+                        }
+                        currentTitle.innerHTML = `<a>${titles[i].innerText}</a>`;
+                        break
+                    }
+                }
+            } else {
+                if (currentTitle) {
+                    currentTitle.remove();
+                }
             }
         }
         // scrollbar
@@ -22,18 +38,18 @@ $(document).ready(function(){
             var c = $(document).height();
             if (c === b) {
                 $(".scrollbar").width("100%");
-            }else {
+            } else {
                 $(".scrollbar").width(a / (c - b) * 100 + "%");
             }
         }
     });
-    e.click(function() {
+    e.click(function () {
         window.scrollTo(0, 0);
     });
     $('[data-fancybox]').fancybox({
         protect: true
     });
-    $(".entry-comment").click(function() {
+    $(".entry-comment").click(function () {
         var dsq = document.createElement('script');
         var thr = document.getElementById('comment_thread')
         dsq.type = 'text/javascript';
@@ -47,16 +63,16 @@ $(document).ready(function(){
         thr.appendChild(dsq);
         $(".entry-comment").parent().fadeToggle(800);
     });
-    $(".navbar-logo").error(function() {
+    $(".navbar-logo").error(function () {
         var content = '<span class="navbar-title">' + $(".navbar-logo").attr("alt") + '</span>';
         $(".navbar-logo").parent().html(content);
     });
-    $(".encrypt-container .encrypt-form input").keyup(function(e){
-      if(e.keyCode == 13) {
-        decrypt($(this).get(0));
-      }
+    $(".encrypt-container .encrypt-form input").keyup(function (e) {
+        if (e.keyCode == 13) {
+            decrypt($(this).get(0));
+        }
     });
-    $(".encrypt-container .encrypt-form i").click(function(e) {
+    $(".encrypt-container .encrypt-form i").click(function (e) {
         decrypt($(this).get(0));
     });
 });
