83ca9dc1112961752ca04e339f92fc9392408f55
honmaple <xiyang0807@gmail.com>
2023-04-09 12:00:32 +0800

    switch to giscus

--- static/lib/js/script.min.js
+++ static/lib/js/script.min.js
@@ -5,12 +5,12 @@ $(document).ready(function () {
     $(window).on("scroll", function () {
         var navbar = $(".navbar");
         var height = $(".navbar").height();
-        var offset = Math.min(document.body.scrollHeight - document.body.offsetHeight, height);
 
-        e.toggleClass("back-to-top-on", window.pageYOffset > 120);
-        navbar.toggleClass("navbar-fixed", window.pageYOffset > offset);
+        e.toggleClass("back-to-top-on", window.pageYOffset >= 120);
+        // 整个文档的高度 - 显示高度 - 锁定后的高度 > 原来navbar的高度
+        navbar.toggleClass("navbar-fixed", window.pageYOffset >= height && document.body.scrollHeight - document.body.offsetHeight - 50 >= height);
 
-        if (window.innerWidth > 1280) {
+        if (window.innerWidth >= 1280) {
             var scrollTop = window.pageYOffset;
             var currentTitle = document.getElementById("navbar-title");
             if (scrollTop > height) {
@@ -51,13 +51,22 @@ $(document).ready(function () {
     });
     $(".entry-comment").click(function () {
         var dsq = document.createElement('script');
-        var thr = document.getElementById('comment_thread')
+        var thr = document.getElementById('comment_thread');
+        var theme = localStorage.getItem("theme") == "theme-tree" ? "dark_dimmed" : "light";
         dsq.type = 'text/javascript';
-        dsq.src = 'https://utteranc.es/client.js';
-        dsq.setAttribute('repo', 'honmaple/honmaple.github.io');
-        dsq.setAttribute('issue-term', thr.getAttribute('issue-term'));
-        dsq.setAttribute('label', 'comment');
-        dsq.setAttribute('theme', 'github-light');
+        dsq.src = 'https://giscus.app/client.js';
+        dsq.setAttribute('data-repo', 'honmaple/honmaple.github.io');
+        dsq.setAttribute('data-repo-id', 'MDEwOlJlcG9zaXRvcnk0MTY4ODc2MQ==');
+        dsq.setAttribute('data-category', 'Announcements');
+        dsq.setAttribute('data-category-id', 'DIC_kwDOAnweuc4CVokl');
+        dsq.setAttribute('data-mapping', 'specific');
+        dsq.setAttribute('data-term', thr.getAttribute('issue-term'));
+        dsq.setAttribute('data-strict', '0');
+        dsq.setAttribute('data-reactions-enabled', '0');
+        dsq.setAttribute('data-emit-metadata', '0');
+        dsq.setAttribute('data-input-position', 'bottom');
+        dsq.setAttribute('data-theme', theme);
+        dsq.setAttribute('data-lang', 'zh-CN');
         dsq.setAttribute('crossorigin', 'anonymous');
 
         thr.appendChild(dsq);
