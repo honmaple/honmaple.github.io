af53869e61f60c2686864f7eda4fc77ba254c1f9
honmaple <xiyang0807@gmail.com>
2023-12-19 12:57:36 +0800

    update some css and js, fix ui

--- static/js/project.js
+++ static/js/project.js
@@ -1,29 +1,26 @@
-$(document).ready(function(){
-    $.get("https://api.github.com/users/honmaple/repos", function(response){
-        let result = response.filter(function(item) {
+$(document).ready(function () {
+    $.get("https://api.github.com/users/honmaple/repos", function (response) {
+        let result = response.filter(function (item) {
             return item.stargazers_count >= 3;
         });
         function Template(item) {
             let pattern = GeoPattern.generate(item.name);
             return `
-<div class="entry-project-content col-sm-4">
-  <div class="thumbnail">
-    <h2 class="entry-background" style=background-image:${pattern.toDataUrl()}><a href="${item.html_url}">${item.name}</a></h2>
-    <div class="caption">
-      <p>${item.description}</p>
-      <hr/>
-      <ul class="list-inline">
-        <li><i class="fa fa-eye"></i><span>${item.watchers_count}</span></li>
-        <li><i class="fa fa-star"></i><span>${item.stargazers_count}</span></li>
-        <li><i class="fa fa-code-fork"></i><span>${item.forks_count}</span></li>
-      </ul>
-    </div>
+<div class="thumbnail">
+  <h2 style=background-image:${pattern.toDataUrl()}><a href="${item.html_url}">${item.name}</a></h2>
+  <div class="caption">
+    <span>${item.description}</span>
+    <ul>
+      <li><i class="fa fa-eye"></i><span>${item.watchers_count}</span></li>
+      <li><i class="fa fa-star"></i><span>${item.stargazers_count}</span></li>
+      <li><i class="fa fa-code-fork"></i><span>${item.forks_count}</span></li>
+    </ul>
   </div>
 </div>`;
         }
         $("#entry-project").html("");
-        for(var i=0,len=result.length;i<len;i+=3){
-            $("#entry-project").append(result.slice(i,i+3).map(Template).join(""));
+        for (var i = 0, len = result.length; i < len; i += 3) {
+            $("#entry-project").append(result.slice(i, i + 3).map(Template).join(""));
         }
     });
 });
--- static/js/timeline.js
+++ static/js/timeline.js
@@ -1,24 +1,24 @@
-$(document).ready(function(){
+$(document).ready(function () {
     if (!String.prototype.format) {
-        String.prototype.format = function() {
+        String.prototype.format = function () {
             var args = arguments;
-            return this.replace(/{(\d+)}/g, function(match, number) {
+            return this.replace(/{(\d+)}/g, function (match, number) {
                 return typeof args[number] != 'undefined'
                     ? args[number]
                     : match
-                ;
+                    ;
             });
         };
     }
     function TimeFormat(time) {
         var date = new Date(time);
         Y = date.getFullYear();
-        M = (date.getMonth()+1 < 10 ? '0'+(date.getMonth()+1) : date.getMonth()+1);
-        D = (date.getDate()+1 < 10 ? '0'+date.getDate() : date.getDate());
+        M = (date.getMonth() + 1 < 10 ? '0' + (date.getMonth() + 1) : date.getMonth() + 1);
+        D = (date.getDate() + 1 < 10 ? '0' + date.getDate() : date.getDate());
         h = date.getHours();
         m = date.getMinutes();
         s = date.getSeconds();
-        return [Y,M,D,h,m,s];
+        return [Y, M, D, h, m, s];
     }
     var template = `
       <li class="timeline-item">
@@ -29,28 +29,27 @@ $(document).ready(function(){
           <span class="timeline-text">{2}</span>
         </div>
       </li>`;
-    $.get("https://honmaple.com/api/timeline",{
-        number:17
-    }, function(response){
+    $.get("https://honmaple.com/api/timeline", {
+        number: 17
+    }, function (response) {
         var first_date = TimeFormat(response.data[0].created_at);
         var first_title = first_date[0] + '年' + first_date[1] + '月';
         var child_template = '';
         var title = '';
         var date = [];
         $("#timeline").html("");
-        response.data.forEach(function(item,index) {
+        response.data.forEach(function (item, index) {
             date = TimeFormat(item.created_at);
             title = date[0] + '年' + date[1] + '月';
             if (first_title != title) {
-                $("#timeline").append('<ul class="timeline timeline-circle"><li class="timeline-count"><h2>{0}</h2>{1}</li></ul>'.format(first_title,child_template));
+                $("#timeline").append('<ul class="timeline timeline-circle"><li class="timeline-count"><h2>{0}</h2>{1}</li></ul>'.format(first_title, child_template));
                 first_title = title;
                 child_template = '';
             }
             child_template += template.format(date[1] + '月' + date[2] + '日', date[2], item.content);
-            console.log(child_template)
         });
         if (child_template) {
-            $("#timeline").append('<ul class="timeline timeline-circle"><li class="timeline-count"><h2>{0}</h2>{1}<li></ul>'.format(first_title,child_template));
+            $("#timeline").append('<ul class="timeline timeline-circle"><li class="timeline-count"><h2>{0}</h2>{1}</li></ul>'.format(first_title, child_template));
         }
     }, 'json');
 });
